e239f3bde975e762b032ea719dbd99014afce743
Beenoo Yadav <beenu.y@coolwinks.com>
2020-12-24 13:01:50 +0530

    elastic as param

--- google_search.py
+++ google_search.py
@@ -1,22 +1,30 @@
+import os
+
+from dotenv import load_dotenv
 from googlesearch import search
 from elasticsearch import Elasticsearch
 
+load_dotenv()
+ELASTIC_NODES = os.getenv('ELASTIC_NODES')
+
+ELASTICSEARCH_CLIENT = Elasticsearch([ipport for ipport in ELASTIC_NODES.split(",")])
+
 
 def get_top5_google_result(query):
-    query_search = ' '.join(query)
+    query_search = ' '.join(query).lower()
     es = ElasSearch()
     es.create_document(query_search)
     return search(query_search, num_results=5)
 
 
 def recent_search_result(query):
-    query_search = ' '.join(query)
+    query_search = ' '.join(query).lower()
     es = ElasSearch()
     return es.get_recent_search(query_search)
 
 
 def search_query(query):
-    return dict(query=dict(regexp=dict(document=dict(value=f".*{query}.*", flags="ALL", case_insensitive=True,
+    return dict(query=dict(regexp=dict(document=dict(value=f".*{query}.*", flags="ALL",
                                                      max_determinized_states=10000, rewrite="constant_score"))))
 
 
@@ -24,7 +32,7 @@ class ElasSearch:
 
     def __init__(self):
         self.index = "recent_search"
-        self.es = Elasticsearch()
+        self.es = ELASTICSEARCH_CLIENT
         self.es.indices.create(index=self.index, ignore=400)
 
     def create_document(self, document):
